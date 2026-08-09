82d804cc4926d033d06ddddf54f526ec12095e30
OmarAwadSaber <01004005001a@gmail.com>
2026-08-09 16:29:44 +0300

    implementing documents logic

--- dyslexia/documents/views.py
+++ dyslexia/documents/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Document
+from rest_framework.permissions import IsAuthenticated
+from .serializers import DocumentSerializer
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+class DocumentListView(generics.ListCreateAPIView):
+    serializer_class = DocumentSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        return get_object_or_404(Document, user=self.request.user).all()
