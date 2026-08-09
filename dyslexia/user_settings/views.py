4cc956250d32fe2e0deaaad7659ff4dcc9abf805
OmarAwadSaber <01004005001a@gmail.com>
2026-08-09 16:14:41 +0300

    implementing usersettings module logic

--- dyslexia/user_settings/views.py
+++ dyslexia/user_settings/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .serializers import UserSettingsSerializer
+from .models import UserSettings
+
+
+class UserSettingsView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSettingsSerializer
+
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return get_object_or_404(UserSettings, user=self.request.user)
