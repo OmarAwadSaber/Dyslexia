c9b8e48b45387018b9d60df21b4db4a153642bbf
OmarAwadSaber <01004005001a@gmail.com>
2026-08-09 16:23:14 +0300

    implementing Document model

--- dyslexia/documents/models.py
+++ dyslexia/documents/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+user_model = get_user_model()
+
+class SOURCE:
+    BROWSER = 'browser'
+    MOBILE = 'mobile'
+    
+    CHOICES = [
+        (BROWSER, 'Browser'),
+        (MOBILE, 'Mobile'),
+    ]
+
+
+class Document(models.Model):
+    user = models.ForeignKey(user_model, on_delete=models.CASCADE)
+    source = models.CharField(max_length=20, choices=SOURCE.CHOICES)
+    raw_text = models.TextField()
+    extracted_at = models.DateTimeField(auto_now_add=True)
