ee096aa35fd8474be2e1aa4766ebfaa200f7ecae
OmarAwadSaber <01004005001a@gmail.com>
2026-08-09 16:32:40 +0300

    adding retrieve and delete endpoints for documents

--- dyslexia/documents/urls.py
+++ dyslexia/documents/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import DocumentListView
+from .views import DocumentListView, DocumentDetailView
 
 
 urlpatterns = [
     path('', DocumentListView.as_view(), name='document-list'),
+    path('<int:pk>/', DocumentDetailView.as_view(), name='document-detail'),
 ]
