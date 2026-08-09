04442eb34f9f843c4bf8fd88015936ce6cfb9a9a
OmarAwadSaber <01004005001a@gmail.com>
2026-08-09 16:00:18 +0300

    implementing UserSettings model

--- dyslexia/user_settings/models.py
+++ dyslexia/user_settings/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+user = get_user_model()
 
 # Create your models here.
+class UserSettings(models.Model):
+    user = models.OneToOneField(user, on_delete=models.CASCADE, related_name='settings')
+    font_type = models.CharField(max_length=255)
+    font_size = models.IntegerField()
+    letter_spacing = models.FloatField()
+    voice_speed = models.FloatField()
+    voice_pitch = models.FloatField()
+    theme = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"Settings for {self.user.username}"
